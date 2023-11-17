af435e9fb90e3ddfa815d867dc22547972596154
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-11-17 09:10:44 +0100

    fix: :label: typing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Toaster from './Toaster.vue';
 import createToaster from './api';
 import Positions from './defaults/positions';
-import type { App } from 'vue';
+import type { App, Plugin } from 'vue';
 import type { Position } from './defaults/positions';
 import type { Options, Type } from './api';
 
@@ -9,13 +9,15 @@ import './themes/default/index.css';
 import './themes/default/colors.css';
 import './themes/default/toast.css';
 
-export default {
-  install: (app : App, options = {}) => {
+export const plugin: Plugin = {
+  install: (app: App, options: Options = {}) => {
     let methods = createToaster(options);
     app.config.globalProperties.$toast = methods;
     app.provide('toast', methods);
   }
 };
+
+export default plugin;
 
 export { Toaster, Positions, createToaster };
 export type { Options, Position, Type };
